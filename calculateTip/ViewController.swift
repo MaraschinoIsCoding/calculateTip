b316e3ade5a2c468d5aad4fdaf973621a064f882
YiranWan <wanyiran_0223@163.com>
2020-10-13 07:25:47 +0800

    Initial Commit

---
+++ calculateTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculateTip
+//
+//  Created by 万亦然 on 2020/10/13.
+//  Copyright © 2020 Maraschino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
